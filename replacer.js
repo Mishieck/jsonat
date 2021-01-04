ae783beb1c63a8499c42b980ead95def7b364df3
Mishieck <mishieckmwale@gmail.com>
2021-01-04 20:37:24 +0200

    Handle Infinity, -Infinity, Date and Symbol.

--- replacer.js
+++ replacer.js
@@ -1,15 +1,35 @@
 export const replacer = (key, value) => (value instanceof Array) ? handleArray(value) : replace(value);
 
 const replace = value => {
+  if (typeof value === "object") {
+    const symbols = Object.getOwnPropertySymbols(value);
+
+    if (symbols[0]) {
+      for (const symbol of symbols) {
+        const val = value[symbol];
+        const description = symbol.description;
+        value["[Symbol(" + description + ")]"] = typeof val === "object" ? Object.assign({}, val) : val;
+        delete value[value];
+      };
+    };
+  };
+
+  const dateRegex = /^\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[0-1])T(?:[0-1]\d|2[0-3]):[0-5]\d:[0-5]\d(?:\.\d+|)(?:Z|(?:\+|\-)(?:\d{2}):?(?:\d{2}))$/;
+
   return (
-    typeof value === "undefined" ? values.undefined()
+    value === undefined ? values.undefined()
+    : value !== value ? values.nan()
+    : value === Infinity ? values.infinity()
+    : value === -Infinity ? values.negativeInfinity()
     : typeof value === "function" ? values.function(value)
     : typeof value === "bigint" ? values.function(value)
-    : value === NaN ? values.nan()
+    : (typeof value === "string" && dateRegex.test(value)) ? values.date(value)
+    : typeof value === "symbol" ? values.symbol(value)
     : value instanceof Boolean ? values.boolean(value)
     : value instanceof Number ? values.number(value)
     : value instanceof String ? values.string(value)
     : value instanceof RegExp ? values.regexp(value)
+    : value instanceof Date ? values.date(value)
     : value
   );
 };
@@ -21,8 +41,12 @@ const values = {
   function: value => "${Function(" + value + ")}",
   bigint: value => "${BigInt(" + value + ")}",
   nan: () => "${NaN}",
-  boolean: value => "${Boolean(" + (value === true ? "true" : "false") + ")}",
+  infinity: () => "${Infinity}",
+  negativeInfinity: () => "${-Infinity}",
+  boolean: value => "${Boolean(" + value + ")}",
   number: value => "${Number(" + value + ")}",
   string: value => "${String(" + value + ")}",
-  regexp: value => "${RegExp(" + value + ")}"
+  regexp: value => "${RegExp(" + value + ")}",
+  date: value => "${Date(" + value + ")}",
+  symbol: value => "${Symbol(" + value.description + ")}"
 };
--- reviver.js
+++ reviver.js
@@ -1,4 +1,20 @@
 export const reviver = (key, value) => {
+  if (typeof value === "object") {
+    const keys = Object.keys(value);
+    const symbolKeyRegex = /^(?:\[Symbol\()([\w\W]+)(?:\)\])$/;
+
+    for (const k of keys) {
+      const matches = k.match(symbolKeyRegex);
+
+      if (matches) {
+        const description = matches[1];
+        const val = value[k];
+        value[Symbol(description)] = typeof val === "object" ? Object.assign({}, val) : val;
+        delete value[k];
+      }
+    }
+  };
+
   return (
     value instanceof Array ? handleArray(value)
     : revive(value)
@@ -6,20 +22,24 @@ export const reviver = (key, value) => {
 };
 
 const revive = value => {
-  const regex = /^\$\{([\w\W]+)[\}]$/;
+  const regex = /^(?:\$\{)([\w\W]+)[\}]$/;
   if (regex.test(value)) {
     const matches = value.match(regex);
     const val = matches[1];
-
+    
     return (
       val === "undefined" ? values.undefined()
       : val === "NaN" ? values.nan()
+      : val === "Infinity" ? values.infinity()
+      : val === "-Infinity" ? values.negativeInfinity()
       : val.startsWith("Function") ? values.function(getInstanceValue(val))
       : val.startsWith("BigInt") ? values.bigint(getInstanceValue(val))
       : val.startsWith("Boolean") ? values.boolean(getInstanceValue(val))
       : val.startsWith("Number") ? values.number(getInstanceValue(val))
       : val.startsWith("String") ? values.string(getInstanceValue(val))
       : val.startsWith("RegExp") ? values.regexp(getInstanceValue(val))
+      : val.startsWith("Date") ? values.date(getInstanceValue(val))
+      : val.startsWith("Symbol") ? values.symbol(getInstanceValue(val))
       : val
     );
   };
@@ -32,12 +52,16 @@ const getInstanceValue = value => value.substring(value.indexOf("(") + 1, value.
 const handleArray = arr => arr.map(value => value instanceof Array ? handleArray(value) : revive(value));
 
 const values = {
-  undefined: () => undefined,
+  undefined: () => null,
   nan: () => NaN,
+  infinity: () => Infinity,
+  negativeInfinity: () => -Infinity,
   function: value => eval(value),
   bigint: value => new BigInt(value),
   boolean: value => new Boolean(value),
   number: value => new Number(value),
   string: value => new String(value),
-  regexp: value => new RegExp(value),
+  regexp: value => new RegExp(value.replace(/^\/|\/$/g, "")),
+  date: value => new Date(value),
+  symbol: value => Symbol(value),
 };
