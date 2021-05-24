a220a40b0863eaa3869cd3cd37951c848638e59a
Mishieck <mishieckmwale@gmail.com>
2021-05-24 16:52:05 +0200

    tests(functionality): add tests for bundle

--- tests/functionality/functionality.js
+++ tests/functionality/functionality.js
@@ -1,6 +1,5 @@
-import { JSONAT, JSONATS } from "/src/main.js"
+import {JSONAT, JSONATS} from "/src/main.js";
 import JAVASCRIPT_DATA_STRUCTURES from "./javascript-data-structures.js";
-
 
 (async () => {
   // console.info(await JSONAT.stringify(JAVASCRIPT_DATA_STRUCTURES));
